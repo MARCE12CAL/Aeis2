f2ab376b2abff94d826809b4f97ae4cd3cb9d97a
MARCE12CAL <calvachemarli@gmail.com>
2024-09-23 21:52:26 -0500

    PoketatanAPI

--- vite-project/src/components/pokemon-list.tsx
+++ vite-project/src/components/pokemon-list.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { PokemonCard } from "./pokemon-card";
 
 type Pokemon = {
@@ -5,50 +6,26 @@ type Pokemon = {
   url: string;
 };
 
-const pokemons: Pokemon[] = [
-    {
-      name: "bulbasaur",
-      url: "https://pokeapi.co/api/v2/pokemon/1/",
-    },
-    {
-      name: "ivysaur",
-      url: "https://pokeapi.co/api/v2/pokemon/2/",
-    },
-    {
-      name: "venusaur",
-      url: "https://pokeapi.co/api/v2/pokemon/3/",
-    },
-    {
-      name: "charmander",
-      url: "https://pokeapi.co/api/v2/pokemon/4/",
-    },
-    {
-      name: "charmeleon",
-      url: "https://pokeapi.co/api/v2/pokemon/5/",
-    },
-    {
-      name: "charizard",
-      url: "https://pokeapi.co/api/v2/pokemon/6/",
-    },
-    {
-      name: "squirtle",
-      url: "https://pokeapi.co/api/v2/pokemon/7/",
-    },
-    {
-      name: "wartortle",
-      url: "https://pokeapi.co/api/v2/pokemon/8/",
-    },
-    {
-      name: "blastoise",
-      url: "https://pokeapi.co/api/v2/pokemon/9/",
-    },
-  ];
-  export function PokemonList() {
-    return (
-      <div>
-        {pokemons.map((pokemon) => (
-          <PokemonCard key={pokemon.url} name ={pokemon.name} url={pokemon.url} />
-        ))}
-      </div>
+export function PokemonList() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  // const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/").then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+        setPokemons(data.results);
+        // No hacer
+        // setNumber(number + 1);
+      })
     );
-  }
+  }, []); // [number]
+
+  return (
+    <div className="grid grid-cols-12 gap-4 mt-8">
+      {pokemons.map((pokemon) => (
+        <PokemonCard key={pokemon.url} name={pokemon.name} url={pokemon.url} />
+      ))}
+    </div>
+  );
+}
